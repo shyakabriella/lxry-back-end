e24e50fe3fee89995128d851c74fc442126f6690
moha_01 <mm7.mansour@gmail.com>
2026-04-24 05:27:26 -0700

    updated

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\HomeAPI\HomePageSectionTwoController;
 
-   
-Route::controller(RegisterController::class)->group(function(){
+
+// Home Page Section Two Routes
+Route::get('/home-page-section-two', [HomePageSectionTwoController::class, 'index']);
+Route::post('/admin/home-page-section-two', [HomePageSectionTwoController::class, 'store']);
+Route::put('/admin/home-page-section-two/{id}', [HomePageSectionTwoController::class, 'update']);
+Route::delete('/admin/home-page-section-two/{id}', [HomePageSectionTwoController::class, 'destroy']);
+
+// 🔐 Auth Routes
+Route::controller(RegisterController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
 });
-         
-Route::middleware('auth:sanctum')->group( function () {
-    
+
+// 🔒 Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    // add protected APIs here later
 });
